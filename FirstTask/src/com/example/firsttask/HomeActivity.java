1ac746fd3b827f24835b5953754ee42d14cd0247
newmorrow <newmorrow@mail.ru>
2014-08-13 13:51:48 +0400

    first part

---
+++ FirstTask/src/com/example/firsttask/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.firsttask;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.a_home_activity);
+	}
+}
